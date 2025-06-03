4fdf53bfa81d0b261b99763835453057fbe57bb3
Rodrigo Alvarez <junior@yetox.com>
2025-06-02 18:03:12 -0600

    Adding security to user routes (token)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, HttpCo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags, ApiResponse, ApiCreatedResponse } from '@nestjs/swagger';
+import { ApiTags, ApiResponse, ApiCreatedResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Users')
@@ -19,38 +19,46 @@ export class UsersController {
     description: 'The record has been successfully created.',
     type: UpdateUserDto,
   })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiBearerAuth()
   @UseGuards(AuthGuard('jwt'))
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   findOne(@Param('id', ParseIntPipe) id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard('jwt'))
   update(@Param('id',ParseIntPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id', ParseIntPipe) id: string) {
     return this.usersService.remove(+id);
   }
 
   @Patch(':id/activateStatus')
+  @UseGuards(AuthGuard('jwt'))
   updateStatusUp(@Param('id', ParseIntPipe) id: string){
     return this.usersService.chageStatusTrue(+id)
   }
 
   @Patch(':id/deactivateStatus')
+  @UseGuards(AuthGuard('jwt'))
   updateStatusDown(@Param('id', ParseIntPipe) id: string){
     return this.usersService.chageStatusFalse(+id)
   }
